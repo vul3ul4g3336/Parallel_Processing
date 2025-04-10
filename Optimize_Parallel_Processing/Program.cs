2109f4fe3b867f883f4c62e3d32c5f9829a41b3d
LAPTOP-543DRTKM\TUF <vul3ul4g3336@gmail.com>
2025-04-10 14:53:27 +0800

    寫入 lock  mutex concurrentBag/concurrentQueue/ReadWriteSlim/SemaphoreSlim(紅綠燈機制)

--- Optimize_Parallel_Processing/Program.cs
+++ Optimize_Parallel_Processing/Program.cs
@@ -1,6 +1,7 @@
 ﻿using CSV_Library;
 using Optimize_Parallel_Processing;
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
@@ -10,6 +11,7 @@ using System.Reflection;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Parallel_Processing
 {
@@ -17,12 +19,16 @@ namespace Parallel_Processing
     public class Program
     {
 
+        private static ConcurrentBag<DataModel> Bag = new ConcurrentBag<DataModel>() { };
+        private static ConcurrentQueue<DataModel> Queue = new ConcurrentQueue<DataModel>() { };
         // async await 是一個必須同時存在的關鍵字
         static async Task Main(string[] args)
         {
-
+            //Mutex mutex = new Mutex();
+            //object lockObject = new object();
+            ReaderWriterLockSlim rwLock = new ReaderWriterLockSlim();
             // cancellation Token => 玩一下這個
-            // lock mutex concurrentBag/concurrentQueue/ReadWriteSlim/SemaphoreSlim(紅綠燈機制)
+            // lock  mutex concurrentBag/concurrentQueue/ReadWriteSlim/SemaphoreSlim(紅綠燈機制)
 
             string readPathDir = @"C:\Users\TUF\source\repos\Parallel_Processing\MockData_Read\MOCK_DATA";
             string writePathDir = @"C:\Users\TUF\source\repos\Parallel_Processing\MockData_Write\MOCK_DATA";
@@ -30,7 +36,7 @@ namespace Parallel_Processing
             List<double> readTimes = new List<double>();
             List<double> writeTimes = new List<double>();
             int batchCount = 3_000_000;
-            int dataCount = 60_000_000; // 200 =>  i=0~200 , 0*50000 , 50000
+            int dataCount = 20_000_000; // 200 =>  i=0~200 , 0*50000 , 50000
             if (Directory.Exists(writePathDir))
                 Directory.Delete(writePathDir, true);
             else
@@ -40,37 +46,56 @@ namespace Parallel_Processing
             int remainder = dataCount % batchCount;
             if (remainder > 0)
                 quotient++;
+
+
             Stopwatch swTotal = new Stopwatch();
             swTotal.Start();
+
             await Parallel.ForAsync(0, quotient, new ParallelOptions { MaxDegreeOfParallelism = 5 }, async (index, token) =>
             {
-
-                Console.WriteLine($"任務{index + 1}啟動!");
                 Stopwatch sw = new Stopwatch();
                 sw.Start();
+                Console.WriteLine($"任務{index + 1}啟動!");
+
+
                 string path = $"{readPathDir}_{dataCount}.csv";
+                rwLock.EnterReadLock();
                 List<DataModel> result = CSV.Read<DataModel>(path
                     , index * batchCount, batchCount);
+                rwLock.ExitReadLock();
                 sw.Stop();
                 double readTime = Math.Round((sw.ElapsedMilliseconds / 1000f), 2);
                 readTimes.Add(readTime);
 
+
                 sw.Restart();
-                CSV.Write($"{writePathDir}_{dataCount}_{index}.csv", result);
-                sw.Stop();
+
+                //lock (lockObject)
+                //mutex.WaitOne();
+
+
+
+
+                //mutex.ReleaseMutex();
+                //sw.Stop();
+                rwLock.EnterWriteLock();
+                CSV.Write($"{writePathDir}_{dataCount}.csv", result);
+                rwLock.ExitWriteLock();
                 double writeTime = Math.Round((sw.ElapsedMilliseconds / 1000f), 2);
 
                 writeTimes.Add(writeTime);
+                Console.WriteLine("任務完成!");
 
                 Console.WriteLine($"第{index + 1}任務完成! 讀取時間:{readTime} | 寫入時間:{writeTime} | 任務完成時間:{Math.Round(readTime + writeTime, 2)} s");
 
                 result.Clear();
                 result = null;
                 GC.Collect();
+
+
             });
 
 
-            Console.WriteLine("任務完成!");
             swTotal.Stop();
             Console.WriteLine($"平均讀取時間:{readTimes.Median()} 平均寫入時間:{writeTimes.Median()} 總完成時間:{Math.Round((swTotal.ElapsedMilliseconds / 1000f), 2)} s");
 
